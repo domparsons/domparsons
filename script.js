7cf08cc7c6e668ce92e530118ba7c50d0dd6bbb1
domparsons <69306241+domparsons@users.noreply.github.com>
2021-03-05 23:10:02 +0000

    Small changes

--- script.js
+++ script.js
@@ -23,12 +23,4 @@ const navSlide = ()=> {
 	});
 }
 
-$('video').each(function(){
-    if ($(this).is(":in-viewport")) {
-        $(this)[0].play();
-    } else {
-        $(this)[0].pause();
-    }
-})
-
 navSlide();
