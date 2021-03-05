0080778d13fa1f94b511deacdf907477047f72d6
domparsons <69306241+domparsons@users.noreply.github.com>
2021-03-05 22:17:24 +0000

    Removed videos

--- script.js
+++ script.js
@@ -23,6 +23,12 @@ const navSlide = ()=> {
 	});
 }
 
+$('video').each(function(){
+    if ($(this).is(":in-viewport")) {
+        $(this)[0].play();
+    } else {
+        $(this)[0].pause();
+    }
+})
 
 navSlide();
-
