b1ca9d42fd92b643e2378bbeb7887ce8191fe16e
domparsons <69306241+domparsons@users.noreply.github.com>
2022-05-25 21:22:28 +0100

    Complete redesign
    New design which is much simpler. Removed all blog posts and photography pages. New portfolio page.

--- scripts/head.js
+++
@@ -1,3 +0,0 @@
-//contains fonts and meta tags but not google analytics scripts
-document.write('<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet"><meta name="viewport" content="width=device-width" />')
-	
